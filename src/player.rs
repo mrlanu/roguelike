6a4f6810aff2abbffdc257a138ba6752bc8d6f3d
mrlanu <mrlanu@gmail.com>
2022-06-12 21:27:09 -0500

    greying out what we remember, but can't see

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,5 @@
 use super::{Player, Position, State, TileType};
-use crate::Map;
+use crate::{Map, Viewshed};
 use rltk::{Rltk, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
@@ -17,16 +17,19 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let mut viewsheds = ecs.write_storage::<Viewshed>();
     let map = ecs.fetch::<Map>();
 
-    for (_player, pos) in (&mut players, &mut positions).join() {
+    for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
         let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
         if map.tiles[destination_idx] != TileType::Wall {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
+
+            viewshed.dirty = true;
         }
     }
 }
